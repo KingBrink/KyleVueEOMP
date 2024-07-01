256c88884b4c24e7127b6929c2c79b8ff4134d42
KingBrink <kyle.brink1602@gmail.com>
2024-07-01 10:58:16 +0200

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class=" body container">
+    <section class="row" id="landing">
+      <LandingSection />
+    </section>
+    <section class="row" id="about">
+      <AboutSection />
+    </section>
+    <section class="row" id="resume">
+      <ResumeSection/>
+
+    </section>
+    <section class="row" id="testimonials">
+      <TestimonialSection/>
+      
+
+    </section>
+    <section class="row" id="project">
+      <ProjectSection/>
+
+    </section>
+    <section class="row" id="reachMe">
+      <ContactSection/>
+
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import LandingSection from '@/components/LandingSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
+import ResumeSection from '@/components/ResumeSection.vue'
+import TestimonialSection from '@/components/TestimonialSection.vue'
+import ProjectSection from '@/components/ProjectSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    LandingSection,
+    AboutSection,
+    ResumeSection,
+    TestimonialSection,
+    ProjectSection,
+    ContactSection
   }
 }
 </script>
+
